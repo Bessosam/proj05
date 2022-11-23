5acb1593c48e054f42c32180dfe97322b1ae03e7
Bessosam <shahoudbassam@gmail.com>
2022-11-23 13:49:33 +0100

    i added two empty files

--- proj05/src/ClassThree.java
+++ proj05/src/ClassThree.java
@@ -1,2 +1,9 @@
 public class ClassThree {
+   void fun(){
+
+System.out.println("this is from master");
+System.out.println("hello hey");
+
+
+    }
 }
--- proj05/src/Classtwo.java
+++ proj05/src/Classtwo.java
@@ -1,2 +1,7 @@
 public class Classtwo {
+  void myfuc(){
+
+
+
+  }
 }

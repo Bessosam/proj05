e90571ac9cb4727c264fae268c6682f6f599273d
Besso <shahoudbassam@gmail.com>
2022-11-30 09:48:47 +0100

    great a spring boot project

--- proj05/src/ClassThree.java
+++
@@ -1,9 +0,0 @@
-public class ClassThree {
-   void fun(){
-
-System.out.println("this is from master");
-System.out.println("hello hey");
-
-
-    }
-}
--- proj05/src/Classtwo.java
+++
@@ -1,7 +0,0 @@
-public class Classtwo {
-  void myfuc(){
-
-
-
-  }
-}
